226982dfe6441679a5ea9072e43ce654c8de947d
GSYDev <gsyteam0@gmail.com>
2015-08-14 18:49:53 -0400

    Genesys initial v1
    GenesysCoin Initial Commit v1

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define GenesysCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "GenesysCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
