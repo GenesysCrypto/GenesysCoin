74f11dd919eb5b5de1bdf22f81ef5b3ef2ffadda
GSYDev <gsyteam0@gmail.com>
2016-03-20 17:35:39 -0400

    Checkpoint Issue Corrected
    Hardcoded checkpoint fixed

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("Genesys-Core");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID ": No-MN"
+#    define GIT_COMMIT_ID ": No-MN2"
 //#    define GIT_COMMIT_ID ": Build01-0116"
 //#    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
